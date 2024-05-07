35238e6956cbe05d1b86fb4b69e2e1fbb55daf85
KamranTalis <az.kamik.rus@gamil.com>
2024-05-08 01:27:26 +0400

    full html css

--- src/App.js
+++ src/App.js
@@ -69,48 +69,68 @@ function App() {
             />
             <button onClick={addTodo}>Add</button>
           </div>
-          <div className="container__lists">
-            {todos.map((todo, index) => (
-              <div
-                className={`container__lists__card ${
-                  todo.isSelected ? "selected" : ""
-                }`}
-                key={index}
-                onClick={() => handleItemClick(index)}
-              >
-                <div className="container__lists__card__left">
-                  <div
-                    className={`circle ${todo.isSelected ? "circle_add" : ""}`}
-                  >
-                    <svg
-                      width="11"
-                      height="9"
-                      viewBox="0 0 11 9"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
+          <div className="contBpxShad">
+            <div className="container__lists">
+              {todos.map((todo, index) => (
+                <div
+                  className={`container__lists__card ${
+                    todo.isSelected ? "selected" : ""
+                  }`}
+                  key={index}
+                  onClick={() => handleItemClick(index)}
+                >
+                  <div className="container__lists__card__left">
+                    <div
+                      className={`circle ${
+                        todo.isSelected ? "circle_add" : ""
+                      }`}
                     >
-                      <path
-                        d="M1 4.3041L3.6959 7L9.6959 1"
-                        stroke="white"
-                        stroke-width="2"
-                      />
-                    </svg>
+                      {" "}
+                      <span>
+                        <svg
+                          width="11"
+                          height="9"
+                          viewBox="0 0 11 9"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M1 4.3041L3.6959 7L9.6959 1"
+                            stroke="white"
+                            stroke-width="2"
+                          />
+                        </svg>
+                      </span>
+                    </div>
+                    <p className={` ${todo.isSelected ? "addP" : ""}`}>
+                      {todo.task}
+                    </p>
                   </div>
-                  <p className={` ${todo.isSelected ? "addP" : ""}`}>
-                    {todo.task}
-                  </p>
+                  <div className="container__lists__card__right">
+                    <i
+                      className="fa-solid fa-x"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        removeTodo(index);
+                      }}
+                    ></i>
+                  </div>
                 </div>
-                <div className="container__lists__card__right">
-                  <i
-                    className="fa-solid fa-x"
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      removeTodo(index);
-                    }}
-                  ></i>
-                </div>
+              ))}
+            </div>
+            <div className="container__bottom">
+              <div className="container__bottom__left">
+                <p>5 items left</p>
               </div>
-            ))}
+              <div className="container__bottom__center">
+                <p>All</p>
+                <p>Active</p>
+                <p>Completed</p>
+              </div>
+              <div className="container__bottom__right">
+                <p>Clear Completed</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
